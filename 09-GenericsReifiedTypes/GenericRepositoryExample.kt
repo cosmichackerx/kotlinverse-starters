4641d0e77f4dd394846982f0f4df703778e50206
cosmichackerx <120310700+cosmichackerx@users.noreply.github.com>
2025-10-22 15:12:14 +0500

    Update GenericRepositoryExample.kt

--- 09-GenericsReifiedTypes/GenericRepositoryExample.kt
+++ 09-GenericsReifiedTypes/GenericRepositoryExample.kt
@@ -1,1 +1,23 @@
+/**
+ * Kotlinverse Starter – Generics & Reified Types
+ * Description: Demonstrates generic class and inline reified function.
+ */
+data class User(val id: Int = 0, val name: String = "Unknown")
 
+class Repository<T>(private val clazz: Class<T>) {
+    fun fetch(): T {
+        return clazz.getDeclaredConstructor().newInstance()
+    }
+}
+
+inline fun <reified T> create(): T {
+    return T::class.java.getDeclaredConstructor().newInstance()
+}
+
+fun main() {
+    val userRepo = Repository(User::class.java)
+    println(userRepo.fetch())
+
+    val newUser: User = create()
+    println(newUser)
+}
