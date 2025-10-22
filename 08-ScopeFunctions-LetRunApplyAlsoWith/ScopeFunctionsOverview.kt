da835ff0b2f0539d5a56135de51d414a31416c00
cosmichackerx <120310700+cosmichackerx@users.noreply.github.com>
2025-10-22 15:11:32 +0500

    Update ScopeFunctionsOverview.kt

--- 08-ScopeFunctions-LetRunApplyAlsoWith/ScopeFunctionsOverview.kt
+++ 08-ScopeFunctions-LetRunApplyAlsoWith/ScopeFunctionsOverview.kt
@@ -1,1 +1,38 @@
+/**
+ * Kotlinverse Starter – Scope Functions: let, run, apply, also, with
+ * Description: Shows side-by-side usage of scope functions.
+ */
+fun main() {
+    val str: String? = "KotlinScope"
 
+    // let
+    str?.let {
+        println("let: $it")
+    }
+
+    // run
+    val runResult = str?.run {
+        println("run context this: $this")
+        "Result from run"
+    }
+    println("runResult: $runResult")
+
+    // apply
+    val sb = StringBuilder().apply {
+        append("Hello ")
+        append("Kotlin")
+    }
+    println("apply result: $sb")
+
+    // also
+    val list = mutableListOf(1, 2, 3).also {
+        println("also before: $it")
+        it.add(4)
+    }
+    println("also after: $list")
+
+    // with
+    with(sb) {
+        println("with: ${this.toString()}")
+    }
+}
